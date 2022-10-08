48a6b89e27c6b022fca1817fa35797fbe53d7648
Faust Fu <faust.fu@gmail.com>
2022-10-09 06:49:01 +0800

    Dev: update PropertyPathFactoryBean case.

--- src/main/java/t15/Main.java
+++ src/main/java/t15/Main.java
@@ -1,4 +1,4 @@
-// 1. 使用 PropertyPathFactoryBean 介面將其他物件成為轉為 beans.
+// 1. 使用 PropertyPathFactoryBean 介面將其他物件的成員成為轉為獨立的 beans 或是直接注入其他 beans.
 package t15;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -8,7 +8,9 @@ public class Main {
     public static void main(String[] args) {
         var ctx = new ClassPathXmlApplicationContext("t15/beans.xml");
         var wife = ctx.getBean("wife", Person.class);
+        var me = ctx.getBean("me", Person.class);
         System.out.println("今天大老婆的自我介紹：");
         wife.introduce();
+        me.introduce();
     }
 }

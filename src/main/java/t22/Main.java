5cec058d159920a6868b720fe470eb5cf5c487e8
Faust Fu <faust.fu@gmail.com>
2022-10-26 22:37:42 +0800

    Dev: add ResourceLoader case.

--- src/main/java/t22/Main.java
+++ src/main/java/t22/Main.java
@@ -1,8 +1,11 @@
 // 1. 使用 Spring 提供的介面 Resource 存取各類資源.
+// 2. ApplicationContext 已實作 ResourceLoader 介面, 提供 方法可取得各類 Resource 物件.
 package t22;
 
 import org.dom4j.Element;
 import org.dom4j.io.SAXReader;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.core.io.Resource;
@@ -13,6 +16,8 @@ public class Main {
         case1();
         case2();
         case3();
+        case4();
+        case5();
     }
 
     private static void case1() throws Exception {
@@ -28,6 +33,18 @@ public class Main {
     private static void case3() throws Exception {
         System.out.println("====case 3====");
         dumpXML(new FileSystemResource("src/main/resources/t22/books.xml")); // 不需再指定 file:
+    }
+
+    private static void case4() throws Exception {
+        System.out.println("====case 4====");
+        var ctx = new ClassPathXmlApplicationContext("t22/beans.xml");
+        dumpXML(ctx.getResource("t22/books.xml"));
+    }
+
+    private static void case5() throws Exception {
+        System.out.println("====case 5====");
+        var ctx = new FileSystemXmlApplicationContext("src/main/resources/t22/beans.xml");
+        dumpXML(ctx.getResource("src/main/resources/t22/books.xml"));
     }
 
     private static void dumpXML(Resource ur) throws Exception {
